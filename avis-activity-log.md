95cd2f442c86e2e19f98ad6d876b179e48ca6c36
avis <antoine.viscardi@gmail.com>
2018-11-13 12:02:47 -0500

    started working on isolating continuous stocks

--- avis-activity-log.md
+++ avis-activity-log.md
@@ -1,22 +1,27 @@
 # Antoine Viscardi's Activity Log
 
 ## 03 Nov 2018
-### Progress
 - Familiarized myself with the competition rules, goals, data and framework.
 
------
 ## 04 Nov 2018
-### Progress
 - Started thinking about how to preprocess and clean the data for the Vanilla Net.
 - Started implementing the cleaning code.
 
------
 ## 05 Nov 2018
-### Progress
 - Look into other kernels exploring the data. Things to consider for futur implementations:
-- Look for data errors: ridiculous spikes. Maybe replace those points with means.
+	- Look for data errors: ridiculous spikes. Maybe replace those points with means.
 - Current implementation: `dropna(inplace=True)` on both dataframe after slicing desired columns and before merging.
-- Finished preprocessing and merging the market and news data.
+- Still struggling to merge market and news data. 
 
-### Thoughts
-After news data was left merged on market data, I realized that not all market data entry have associated news (which is totally normal and expected). However, **how are we going to deal with this missing data?**
+## 12 Nov 2018 - Meeting
+- How do we deal with missing data?
+	- Filter stock with only continuous data (market and news)
+	- Make sure we only have continuous data (news + market), that is, filter out stock with missing data.
+	- If data is very sparse, 
+		- Average over a week / months
+		- Could engineer a feature with the number of headlines included / sum of the relevances / etc.
+		- Don't spend more than 2-3 hours on this.
+
+## 13 Nov 2018 
+- Merged all branches to master, everyone should branch off from there and avoid deleting files
+- Created python script for data cleaning (as opposed to doing it inside notebooks).
---
+++ models/data_cleaning.py
@@ -0,0 +1,146 @@
+import numpy as np
+import pandas as pd
+from itertools import chain
+
+REDUCED = True
+MARKET_DATA_PATH = "../data/raw/market_train_df.csv"
+NEWS_DATA_PATH = "../data/raw/news_train_df.csv"
+
+def clean_market_data(market_df, train=True):
+    '''Clean and preprocess the market data for training or testing.
+    
+    Parameters
+    ----------
+    market_df : dataframe
+        See https://www.kaggle.com/c/two-sigma-financial-news/data for full 
+        description of the dataframe.
+    train : bool
+        When true, adds the target variable to the dataframe.
+    
+    Returns
+    -------
+    dataframe 
+        Cleaned market data.
+    
+    '''
+    # Select columns and drop NA
+    if train:
+        cols = ['assetCode', 'time', 'volume', 'open', 'returnsOpenPrevMktres1',
+                'returnsOpenPrevMktres10', 'returnsOpenNextMktres10']
+    else:
+        cols = ['assetCode', 'time', 'volume', 'open', 'returnsOpenPrevMktres1',
+                'returnsOpenPrevMktres10']
+    market_df = market_df.loc[:,cols]
+    market_df.dropna(inplace=True)
+    
+    # Normalize time
+    market_df.loc[:, 'time'] = market_df.time.dt.normalize()
+    
+    return market_df
+
+
+def clean_news_data(news_df):
+    '''Clean and preprocess the news data for training or testing.
+    
+    Parameters
+    ----------
+    news_df : dataframe
+        See https://www.kaggle.com/c/two-sigma-financial-news/data for full 
+        description of the dataframe.
+    
+    Returns
+    -------
+    dataframe 
+        Cleaned news data.
+    
+    '''
+    # Select columns and drop NA
+    cols = ['time','assetCodes', 'sentimentNegative', 'sentimentNeutral', 
+            'sentimentPositive', 'urgency', 'provider', 'bodySize', 'relevance']
+    news_df = news_df.loc[:,cols]
+    news_df.dropna(inplace=True)
+    
+    # Normalize time
+    news_df.loc[:, 'time'] = news_df.time.dt.normalize()
+    
+    # assetCodes from String to List
+    news_df['assetCodes'] = news_df['assetCodes'].str.findall(f"'([\w\./]+)'")
+    
+    # Explode news on assetCodes
+    assetCodes_expanded = list(chain(*news_df['assetCodes']))
+    assetCodes_index = news_df.index.repeat(news_df['assetCodes'].apply(len))
+
+    assert len(assetCodes_expanded) == len(assetCodes_index)
+    
+    assetCodes_df =  pd.DataFrame({'index': assetCodes_index, 'assetCode': assetCodes_expanded})
+    news_df_exploded = news_df.merge(assetCodes_df, 'right', right_on='index', left_index=True, validate='1:m')
+    news_df_exploded.drop(['assetCodes', 'index'], 1, inplace=True)
+
+    if extra_features:
+        # Compute means for same date and assetCode
+        news_agg_dict = {
+            'sentimentNegative':'mean',
+            'sentimentNeutral':'mean',
+            'sentimentPositive':'mean',
+            'urgency':'mean',
+            'bodySize':'mean',
+            'relevance':'mean'
+            }
+        news_df_agg = news_df_exploded.groupby(['time', 'assetCode'], as_index=False).agg(news_agg_dict)
+        
+        # Add provider information
+        idx = news_df_exploded.groupby(['time', 'assetCode'])['urgency'].transform(max) == news_df_exploded['urgency']
+        news_df_exploded_2 = news_df_exploded[idx][['time', 'assetCode', 'provider']].drop_duplicates(['time', 'assetCode'])
+        news_df_agg = news_df_agg.merge(news_df_exploded_2, 'left', ['time', 'assetCode'])
+        
+        # One-hot encoding provider
+        ohe_provider = pd.get_dummies(news_df_agg['provider'])
+        news_df_agg = pd.concat([news_df_agg, ohe_provider], axis=1).drop(['provider'], axis=1)
+    
+    else:
+        # Compute means for same date and assetCode
+        news_agg_dict = {
+            'sentimentNegative':'mean',
+            'sentimentNeutral':'mean',
+            'sentimentPositive':'mean'
+            }
+        news_df_agg = news_df_exploded.groupby(['time', 'assetCode'], as_index=False).agg(news_agg_dict)
+    
+    return news_df_agg
+
+
+def clean_data(market_df, news_df, train=True):
+    '''Clean and preprocess the news and market data for training then merge them, to create a train set or test set.
+    
+    Parameters
+    ----------
+    market_df : dataframe
+        See https://www.kaggle.com/c/two-sigma-financial-news/data for full description of the dataframe.
+    news_df : dataframe
+        See https://www.kaggle.com/c/two-sigma-financial-news/data for full description of the dataframe.
+    train : bool
+        When true, creates both the input features and the target dataframes.
+    extra_features : bool
+        When true, adds extra columns that SE added ('urgency', 'provider', 'bodySize', 'relevance').
+        
+    Returns
+    -------
+    dataframe 
+        Cleaned data ready to be fed to the model. Returns both the input and the target dataframes when train=True.
+    
+    '''
+    cleaned_market_df = clean_market_data(market_df, train)
+    cleaned_news_df = clean_news_data(news_df)
+    
+    # Merge on market data
+    df_merged = cleaned_market_df.merge(cleaned_news_df, 'left', ['time', 'assetCode'])
+    
+    if train:
+        y = df_merged['returnsOpenNextMktres10']
+        X = df_merged.drop(['returnsOpenNextMktres10'], axis=1)
+        return X, y
+    else:
+        return df_merged
+
+if __name__ == '__main__':
+    X_train, y_train = clean_data(market_train_df, news_train_df)
