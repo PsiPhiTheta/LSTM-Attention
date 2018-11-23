dfd4bd7802a81fb92a6749b8de7ab131c22039ec
avis <antoine.viscardi@gmail.com>
2018-11-23 13:41:13 -0500

    moved files to dirs

--- models/data_cleaning.py
+++ models/data_cleaning.py
@@ -1,9 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[40]:
+
+
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 from itertools import chain
 
-MARKET_DATA_PATH = "../data/raw/market_train_df.csv"
-NEWS_DATA_PATH = "../data/raw/news_train_df.csv"
+
+# In[8]:
+
+
+MARKET_DATA_PATH = "./data/raw/market_train_df.csv"
+NEWS_DATA_PATH = "./data/raw/news_train_df.csv"
+
+
+# In[4]:
+
 
 def clean_market_data(market_df, train=True):
     '''Clean and preprocess the market data for training or testing.
@@ -36,14 +52,16 @@ def clean_market_data(market_df, train=True):
 
     # Filter out stocks that cover the full time series
     series_len = market_df.time.nunique()
-    market_df = market_df.groupby('assetCode') \
-                         .filter(lambda x: len(x) == series_len)
+    market_df = market_df.groupby('assetCode')                          .filter(lambda x: len(x) == series_len)
     assert (market_df.groupby('assetCode').size() == series_len).all()
     
     # Normalize time
     market_df.loc[:, 'time'] = pd.to_datetime(market_df.time).dt.normalize()
     
     return market_df
+
+
+# In[5]:
 
 
 def clean_news_data(news_df):
@@ -104,6 +122,10 @@ def clean_news_data(news_df):
 
     return news_df_agg
 
+
+# In[27]:
+
+
 def clean_data(market_df, news_df, train=True):
     '''Clean and preprocess the news and market data for training then merge them, to create a train set or test set.
     
@@ -134,10 +156,161 @@ def clean_data(market_df, news_df, train=True):
         return X, y
     else:
         return df_merged
+
+
+# In[51]:
+
+
+def EDA():
+    '''Prints a brief overview of the data.
+    
+    Parameters
+    ----------
+    none
+
+    Returns
+    -------
+    none
+    
+    '''
+    print(X_train.shape, y_train.shape)
+    print(y_train.head())
+    print(X_train.head())
+    print(X_train.info())
+    print(X_train.describe())
+
+
+# In[82]:
+
+
+def plot_asset(market, assetCode):
+    '''Plots an asset's price, volatility and voume.
+    
+    Parameters
+    ----------
+    market_df : dataframe
+        See https://www.kaggle.com/c/two-sigma-financial-news/data for full description of the dataframe.
+    assetCode : string
+        The asset code of the instrument you want to plot
+
+    Returns
+    -------
+    none
+    
+    '''
+    # Set plot style
+    plt.style.use('seaborn')
+    
+    # Fetch the asset from data
+    ass_market = market[market['assetCode'] == assetCode]
+    ass_market.index = ass_market.time
+
+    # Setup 3 subplots
+    f, axs = plt.subplots(2,1, sharex=True, figsize=(12,8))
+    
+    # Subplot 1. Close price 
+    ass_market.close.plot(ax=axs[0], color='black')
+    axs[0].set_ylabel("Price")
+
+    # Subplot 2. Volatility 
+    volat_df = (ass_market.close - ass_market.open)
+    (ass_market.close - ass_market.open).plot(color='darkred', ax = axs[1])
+    axs[1].set_ylabel("Volatility")
+
+    # Show all subplots with label
+    f.suptitle("Asset: %s" % assetCode, fontsize=22)
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.93)
+    plt.show()
+
+
+# In[131]:
+
+
+def plot_chosen_assets():
+    '''Prints a group of select stocks, their price and their volatility.
+    
+    Parameters
+    ----------
+    none
+
+    Returns
+    -------
+    none
+    
+    '''
+    # Huge stocks (market cap 200BN - 1000BN)
+    #plot_asset(market_train_df, "GOOGL.O") #nonsense data?
+    #plot_asset(market_train_df, "AAPL.O") #randomly crashes from 2013-2015?
+    #plot_asset(market_train_df, "FB.O") #Facebook: correct, verified, unpredictable volatility
+    plot_asset(market_train_df, "INTC.O") #Intel: correct, verified, fair constant volatility
+    plot_asset(market_train_df, "WFC.N") #Wells Fargo: correct, verified, crash volatility
+    plot_asset(market_train_df, "AMZN.O") #Amazon: correct, verified, increasing volatility
+    
+    # SMEs (5-20Bn MC)
+    #plot_asset(market_train_df, "ADI.N") #Analogue Devices (32Bn MC): kinda correct (one weird correction), verified
+    #plot_asset(market_train_df, "NATI.O") #NI (6Bn MC): kinda correct (one weird correction in middle), verified
+    plot_asset(market_train_df, "A.N") #Agilent Tech (20Bn MC): kinda correct (one weird correction toward end), verified
         
+    # Small stocks (MC < 1Bn)
+    #plot_asset(market_train_df, "ANDE.O") #Andersons (900M MC): unverified, high vol
+    #plot_asset(market_train_df, "ALO.N") #Alio Gold (90M MC): unverified, low vol
+    plot_asset(market_train_df, "BHE.N") #Benchmark Electronics (1Bn MC): verified, low vol
 
-if __name__ == '__main__':
 
-    market_train_df = pd.read_csv(MARKET_DATA_PATH)
-    news_train_df = pd.read_csv(NEWS_DATA_PATH)
-    X_train, y_train = clean_data(market_train_df, news_train_df)
+# In[138]:
+
+
+def extract_stock(X_train, y_train, assetCode):
+    '''Extracts the training data for a particular asset
+    
+    Parameters
+    ----------
+    X_train : pandas dataframe containing all the assets' training data
+    y_train : pandas dataframe containing all the assets' labels
+    assetCode: asset code of asset to be extracted 
+
+    Returns
+    -------
+    X_train_asset : pandas dataframe containing data for only the chosen assetCode
+    y_train_asset : pandas dataframe containing label for only the chosen assetCode
+    '''
+    X_train_asset = X_train[X_train['assetCode']==assetCode]
+    y_train_asset = X_train.join(y_train)
+    y_train_asset = y_train_asset[y_train_asset['assetCode']==assetCode]
+    y_train_asset = y_train_asset.T.tail(1)
+    
+    return X_train_asset, y_train_asset
+
+
+# In[95]:
+
+
+market_train_df = pd.read_csv(MARKET_DATA_PATH)
+news_train_df = pd.read_csv(NEWS_DATA_PATH)
+X_train, y_train = clean_data(market_train_df, news_train_df)
+
+print("Data cleaned!") #can take a while
+
+
+# In[96]:
+
+
+EDA()
+
+
+# In[133]:
+
+
+plot_chosen_assets()
+
+
+# In[139]:
+
+
+X_train_INTC, y_train_INTC = extract_stock(X_train, y_train, "INTC.O")
+X_train_WFC, y_train_WFC = extract_stock(X_train, y_train, "WFC.N")
+X_train_AMZN, y_train_AMZN = extract_stock(X_train, y_train, "AMZN.O")
+X_train_A, y_train_A = extract_stock(X_train, y_train, "A.N") 
+X_train_BHE, y_train_BHE = extract_stock(X_train, y_train, "BHE.N") 
+
